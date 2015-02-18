a424b4f087cd7f3f572b70ee3b25f1a841cb165a
Arnaud Frugier <frugier.arnaud@gmail.com>
2015-02-18 14:15:02 -0800

    Runs code un main/other thread

--- app/src/main/java/com/quanturium/testbugprocselfexe/MyActivity.java
+++ app/src/main/java/com/quanturium/testbugprocselfexe/MyActivity.java
@@ -3,6 +3,7 @@ package com.quanturium.testbugprocselfexe;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 
 import java.io.File;
@@ -16,15 +17,27 @@ public class MyActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		getPathOfExecutable();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run ()
+			{
+				getPathOfExecutable();
+			}
+		}).start();
+	}
+
+	private void getPathOfExecutable()
+	{
 		try
 		{
-			Log.e("DBG ACTIVITY", "path: " + new File("/proc/self/exe").getCanonicalFile().getPath());
+			Log.e("DBG", "Thread: " + Thread.currentThread().getName());
+			Log.e("DBG", "Path: " + new File("/proc/self/exe").getCanonicalFile().getPath()); // Triggers a crash if executed in a different thread than main thread. Works great on main thread
 		}
 		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
-
-		startService(new Intent(this, MyService.class));
 	}
 }
